9ad9138db0409191e9ffe0dad0929ceb1136cfc1
Roblox Thot <67937010+Roblox-Thot@users.noreply.github.com>
2025-02-11 15:22:05 +0000

    rip

--- games/universal.lua
+++ games/universal.lua
@@ -14,7 +14,7 @@ end
 local function downloadFile(path, func)
 	if not isfile(path) then
 		local suc, res = pcall(function()
-			return game:HttpGet('https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/'..readfile('newvape/profiles/commit.txt')..'/'..select(1, path:gsub('newvape/', '')), true)
+			return game:HttpGet('https://raw.githubusercontent.com/Roblox-Thot/VapeV4ForRoblox/'..readfile('newvape/profiles/commit.txt')..'/'..select(1, path:gsub('newvape/', '')), true)
 		end)
 		if not suc or res == '404: Not Found' then
 			error(res)
@@ -563,14 +563,7 @@ run(function()
 	end
 
 	function whitelist:update(first)
-		local suc = pcall(function()
-			local _, subbed = pcall(function()
-				return game:HttpGet('https://github.com/7GrandDadPGN/whitelists')
-			end)
-			local commit = subbed:find('currentOid')
-			commit = commit and subbed:sub(commit + 13, commit + 52) or nil
-			commit = commit and #commit == 40 and commit or 'main'
-			whitelist.textdata = game:HttpGet('https://raw.githubusercontent.com/7GrandDadPGN/whitelists/'..commit..'/PlayerWhitelist.json', true)
+			whitelist.textdata = '{"WhitelistedUsers":{"378746510596243458":{"hash":"'none","attackable":false,"level": 0,"tags":[{"text":"AC Mod","color":[160,34,160]}]}},"WhitelistedTags":{"378746510596243458":[{"text":"AC Mod","color":[160,34,160]}]},"BlacklistedUsers":{},"Announcement":{"text":"","expiretime":0,"targets":"a"},"KillVape":false}'
 		end)
 		if not suc or not hash or not whitelist.get then return true end
 		whitelist.loaded = true
